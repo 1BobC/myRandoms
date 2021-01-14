5a7ba77712566bc80bb33a6e8aca12467ed71e64
Bob.C <bob.cocksedge@gmail.com>
2021-01-14 15:19:34 +0000

    Tidied up script by including calculation in each number's call back

--- README.md
+++ README.md
@@ -11,3 +11,5 @@ Create a random number for each item in the array
 Ensure each random number is different.
 
 "Completed coding for 6 random numbers simply displayed on document" 
+"Tidied up jscipt reducing number of lines by including random number calculation
+in each number's call back code"
--- sandbox.js
+++ sandbox.js
@@ -1,46 +1,26 @@
-let random= Math.random();
-//console.log(Math.round(random * 50));
 
-
-let one= Math.random();
-///console.log(Math.round(one * 50));
-
-let two= Math.random();
-//console.log(Math.round(two * 50));
-
-let three= Math.random();
-//console.log(Math.round(three * 50));
-
-let four= Math.random();
-//console.log(Math.round(four * 50));
-
-let five= Math.random();
-//console.log(Math.round(five * 50));
-
-let six= Math.random();
-//console.log(Math.round(six * 50));
 
 let randies= document.querySelectorAll("li");
-//console.log(randies);
+
 randies.forEach(randy => {
     if(randy.textContent.includes('First')){
-        randy.innerText += Math.round(one * 50);  
+        randy.innerText += " is  " +  Math.round(Math.random() * 50); 
     }
     if(randy.textContent.includes('Second')){
-    randy.innerText += Math.round(two * 50);  
-}
-   if(randy.textContent.includes('Third')){
-    randy.innerText += Math.round(three * 50);
-   } 
-if(randy.textContent.includes('Fourth')){
-    randy.innerText += Math.round(four * 50);
-   }  
-if(randy.textContent.includes('Fifth')){
-    randy.innerText += Math.round(five * 50);
-   } 
+        randy.innerText += " is  " +  Math.round(Math.random() * 50);  
+ }
+    if(randy.textContent.includes('Third')){
+        randy.innerText += " is  " +  Math.round(Math.random() * 50);
+} 
+ if(randy.textContent.includes('Fourth')){
+        randy.innerText += " is  " + Math.round(Math.random() * 50);
+}  
+ if(randy.textContent.includes('Fifth')){
+        randy.innerText += " is  " +  Math.round(Math.random() * 50);
+} 
 if(randy.textContent.includes('Sixth')){
-    randy.innerText += Math.round(six * 50);
-   }     
+     randy.innerText += " is  " +  Math.round(Math.random() * 50);
+}     
 });
 
 
