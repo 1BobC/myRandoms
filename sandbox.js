8d0e738760aa804b067109b4f2d83303eec4fb14
roco <bob.cocksedge@gmail.com>
2021-05-12 20:44:48 +0100

    Completed coding for 6 random nubers 1-59 and 5 random numbers 1-47 displayed in html

--- sandbox.js
+++ sandbox.js
@@ -4,23 +4,43 @@ let randies= document.querySelectorAll("li");
 
 randies.forEach(randy => {
     if(randy.textContent.includes('First')){
-        randy.innerText += " is  " +  Math.round(Math.random() * 50); 
+        randy.innerText += " is  " +  Math.round(Math.random() * 47); 
     }
     if(randy.textContent.includes('Second')){
-        randy.innerText += " is  " +  Math.round(Math.random() * 50);  
+        randy.innerText += " is  " +  Math.round(Math.random() * 47);  
  }
     if(randy.textContent.includes('Third')){
-        randy.innerText += " is  " +  Math.round(Math.random() * 50);
+        randy.innerText += " is  " +  Math.round(Math.random() * 47);
 } 
  if(randy.textContent.includes('Fourth')){
-        randy.innerText += " is  " + Math.round(Math.random() * 50);
+        randy.innerText += " is  " + Math.round(Math.random() * 47);
 }  
  if(randy.textContent.includes('Fifth')){
-        randy.innerText += " is  " +  Math.round(Math.random() * 50);
+        randy.innerText += " is  " +  Math.round(Math.random() * 47);
 } 
-if(randy.textContent.includes('Sixth')){
-     randy.innerText += " is  " +  Math.round(Math.random() * 50);
-}     
+if(randy.textContent.includes('first')){
+    randy.innerText += " is  " +  Math.round(Math.random() * 59); 
+}
+if(randy.textContent.includes('second')){
+    randy.innerText += " is  " +  Math.round(Math.random() * 59);  
+}
+if(randy.textContent.includes('third')){
+    randy.innerText += " is  " +  Math.round(Math.random() * 59);
+} 
+if(randy.textContent.includes('fourth')){
+    randy.innerText += " is  " + Math.round(Math.random() * 59);
+}  
+if(randy.textContent.includes('fifth')){
+    randy.innerText += " is  " +  Math.round(Math.random() * 59);
+} 
+if(randy.textContent.includes('sixth')){
+    randy.innerText += " is  " +  Math.round(Math.random() * 59);
+}
 });
+
+
+
+
+
 
 
